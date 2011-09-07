94b1f50eb784445e20f4d908a9c92bb8191ac54f
Todd Treangen <treangen@gmail.com>
2011-09-07 18:25:25 -0400

    fixed paths

--- Test/test_filter_interleaved_fastq.sh
+++ Test/test_filter_interleaved_fastq.sh
@@ -1,2 +1,2 @@
-python createProject.py -q -m test12.fq -d test12 -f -i 150:450
-python runPipeline.py -c fcp -p 4 -d test12 -k 61 -t -f Preprocess
+python ../createProject.py -q -m test12.fq -d test12 -f -i 150:450
+python ../runPipeline.py -c fcp -p 4 -d test12 -k 61 -t -f Preprocess
--- Test/test_filter_noninterleaved_fastq.sh
+++ Test/test_filter_noninterleaved_fastq.sh
@@ -1,2 +1,2 @@
-python createProject.py -q -1 test1.fq -2 test2.fq -d test12 -i 150:450
-python runPipeline.py -c fcp -p 4 -d test12 -k 61 -t -f Preprocess
+python ../createProject.py -q -1 test1.fq -2 test2.fq -d test12 -i 150:450
+python ../runPipeline.py -c fcp -p 4 -d test12 -k 61 -t -f Preprocess

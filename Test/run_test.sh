eb37de2149410233892e2efcf9806c3286234709
Todd Treangen <treangen@gmail.com>
2011-09-07 18:23:02 -0400

    fixed paths

--- Test/run_test.sh
+++ Test/run_test.sh
@@ -1,3 +1,3 @@
 #/bin/sh
-python createProject.py -m carsonella_pe_filt.fna -d test1 -f -i 150:450
-python runPipeline.py -c fcp -p 4 -d test1 -k 61 
+python ../createProject.py -m carsonella_pe_filt.fna -d test1 -f -i 150:450
+python ../runPipeline.py -c fcp -p 4 -d test1 -k 61 

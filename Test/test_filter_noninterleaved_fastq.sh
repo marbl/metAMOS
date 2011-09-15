e194dbf0ab28616d3c2ce5a6d273070ca11b0251
Todd Treangen <treangen@gmail.com>
2011-09-15 13:55:00 -0400

    removed .py

--- Test/test_filter_noninterleaved_fastq.sh
+++ Test/test_filter_noninterleaved_fastq.sh
@@ -1,2 +1,2 @@
 ../createProject.py -q -1 test1.fq -2 test2.fq -d test12 -i 150:450
-../runPipeline.py -c fcp -p 4 -d test12 -k 61 -t -f Preprocess
+../runPipeline -c fcp -p 4 -d test12 -k 61 -t -f Preprocess

afae1d3904e399a3466f91df80d5fc9638e24345
Todd Treangen <treangen@gmail.com>
2011-09-15 17:26:49 -0400

    change default to metaphyler

--- Test/run_test2.sh
+++ Test/run_test2.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 ../createProject -m MA_test2.filt2.fna -d test2 -f -i 1300:1700
-../runPipeline -c fcp -p 8 -d test2 -k 31 -f Scaffold,Propagate,Classify -v
+../runPipeline -c metaphyler -p 8 -d test2 -k 31 -f Scaffold,Propagate,Classify -v

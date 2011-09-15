b6ae04b11e21eb0a2040134c2c44263aabc31eb8
Todd Treangen <treangen@gmail.com>
2011-09-15 16:27:50 -0400

    remove version check, place in INSTALL.py

--- setup.py
+++ setup.py
@@ -2,10 +2,6 @@ import sys
 import os
 from setuptools import setup
 
-if (sys.version_info[0] < 2) or (sys.version_info[0] == 2 and sys.version_info[1] < 6): 
-  print "Python version is %s. metAMOS requires at least 2.6"%(sys.version)
-  sys.exit(1)
-
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw

237ea2c9fc54973d26a453aeb7186b3f3bdd28e7
Todd Treangen <treangen@gmail.com>
2011-12-07 13:35:39 -0500

    make soap default asm for this test

--- Test/test_amphora.sh
+++ Test/test_amphora.sh
@@ -1,2 +1,2 @@
 ../initPipeline -f -m carsonella_pe_filt.fna -d test_amphora  -i 150:450
-../runPipeline -c amphora -p 4 -d test_amphora -k 45 -t -f Preprocess
+../runPipeline -a soap -c amphora -p 4 -d test_amphora -k 45 -t -f Preprocess

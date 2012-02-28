2840ce2196a51b61500d56d525a8d810a054e5f8
Sergey Koren <sergek@umd.edu>
2012-02-28 16:06:11 -0500

    Default threads to number of available CPUs on a machine Fix bug filtering fasta sequences

--- src/runPipeline.py
+++ src/runPipeline.py
@@ -1,9 +1,10 @@
 #!python
 
-import os, sys, string, time, BaseHTTPServer, getopt, re, subprocess, webbrowser
+import os, sys, string, time, BaseHTTPServer, getopt, re, subprocess, webbrowser, multiprocessing
 from operator import itemgetter
 
 INITIAL_SRC   = "%s%ssrc"%(sys.path[0], os.sep)
+DEFAULT_KMER  = 31
 
 sys.path.append(INITIAL_SRC)
 import utils
@@ -68,7 +69,7 @@ fqlibs = {}
 fqfrags = []
 rlibs = []
 mapper = "bowtie"
-settings = utils.Settings(31, 16, "")
+settings = utils.Settings(DEFAULT_KMER, multiprocessing.cpu_count() - 1, "")
 
 for o, a in opts:
     if o in ("-v","--verbose"):
@@ -237,7 +238,8 @@ for line in inf:
     elif "frg" in line:
 
         data = line.split("\t")
-        frg = data[1]
+        #frg = data[1]
+        frg = "%s/Preprocess/in/%s"%(settings.rundir,data[1].split(",")[0])
         mated = False
         f1 = frg
         #fqfrags[data[0]] = data[1]

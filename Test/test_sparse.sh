f8853080206eedc6a57d8296bbc396ac7a8c7832
Sergey Koren <sergek@umd.edu>
2012-03-06 10:02:58 -0500

    Add test for minimus assembler and updated sparseassembler test

--- Test/test_sparse.sh
+++ Test/test_sparse.sh
@@ -1,3 +1,3 @@
 #bzip2 -d test4.sff.bz2
-../initPipeline -l flx -q -m carsonella_pe_filt.fq -d test_sparse -i 3000:4000 
+../initPipeline -l flx -f -m carsonella_pe_filt.fna -d test_sparse -i 3000:4000 
 ../runPipeline -c phmmer -p 8 -a sparseassembler -d test_sparse -k 55 -t -f Preprocess

1690a28bc98deaf6370c9d23820c43e44cb62ebd
Sergey Koren <sergek@umd.edu>
2012-06-14 15:45:32 -0400

    Update test to test retaining bank

--- Test/test_fcp.sh
+++ Test/test_fcp.sh
@@ -1,3 +1,3 @@
 #bzip2 -d test4.sff.bz2
 ../initPipeline -l flx -f -m carsonella_pe_filt.fna -d test_fcp -i 3000:4000 
-../runPipeline -v -c fcp -p 8 -a soap -d test_fcp -k 55 -t -f Preprocess -z phylum
+../runPipeline -r -v -c fcp -p 8 -a soap -d test_fcp -k 55 -t -f Preprocess -z phylum

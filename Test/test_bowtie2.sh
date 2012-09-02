8fdba1ab76c83d0b982dde6ec05c401c187adbc7
Todd Treangen <treangen@gmail.com>
2012-09-02 00:08:36 -0400

    changes to accomadate bowtie2 support

---
+++ Test/test_bowtie2.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+../initPipeline -f -m carsonella_pe_filt.fna -d test1  -i 500:3500
+../runPipeline -a soap -m bowtie2 -c fcp -g fraggenescan -p 15 -d test1 -k 45 -t -f Assemble,MapReads,FindORFS,FindScaffoldORFS,Abundance,Classify,Annotate

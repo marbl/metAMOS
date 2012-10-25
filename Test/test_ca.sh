fcc354dcf08cb9d84348d87e9c35e10130ed1b20
Sergey Koren <sergek@umd.edu>
2012-10-25 09:41:19 -0400

    Update CA tests to use FCP

--- Test/test_ca.sh
+++ Test/test_ca.sh
@@ -1,3 +1,3 @@
 #bzip2 -d test4.sff.bz2
 ../initPipeline -l flx -q -m carsonella_pe_filt.fq -d test_ca -i 3000:4000 
-../runPipeline -c phylosift -a ca -p 4 -d test_ca -k 55 -t -f Preprocess
+../runPipeline -c fcp -a ca -p 4 -d test_ca -k 55 -t -f Preprocess

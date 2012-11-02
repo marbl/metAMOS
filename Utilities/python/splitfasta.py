49648456ce3ae5ebe3347b47e99670d0a9f9a54b
Sergey Koren <sergek@umd.edu>
2012-11-02 14:57:04 -0400

    Update split fasta script to take more parameters (# bases per fasta, output file name, etc) Add a master NCBI translate file

--- Utilities/python/splitfasta.py
+++ Utilities/python/splitfasta.py
@@ -2,35 +2,40 @@ import os, string, sys
 
 if __name__ == "__main__":
     r = open(sys.argv[1],'r')
-    seqs = []
+    seqs = {}
     prevseq = ""
+    header = ""
     for line in r:
         if ">" in line:
             if prevseq != "":
-                seqs.append(prevseq)
-            prevseq = line
+                seqs[header] = prevseq
+            prevseq = ""
+            header = line
         else:
             prevseq += line
             
+    if len(prevseq) > 0:
+       seqs[header] = prevseq
 
     cnt = 1
     curlen = 0
+    maxlen = int(sys.argv[2])
+    prefix = sys.argv[1]
+    if (len(sys.argv) > 3):
+       prefix = sys.argv[3]
+       cnt = int(sys.argv[4])
 
-    r = open(sys.argv[1]+"_part%d.fa"%(cnt),'w')            
-    for seq in seqs:
-
-        if curlen+len(seq) <= 6000000:
-            r.write(seq)
-            curlen+= len(seq)-2
+    r = open(prefix+"_part%d.fa"%(cnt),'w')            
+    for id in seqs.keys():
+        seq = seqs[id] 
+        if curlen <= maxlen:
+            r.write(id + seq)
+            curlen+= len(seq)
         else:
             cnt +=1
             curlen = 0
-            r = open(sys.argv[1]+"_part%d.fa"%(cnt),'w')
-            r.write(seq)
-            curlen+= len(seq)-2
-    
-            
-            
-        
-    
-    
+            r.close()
+            r = open(prefix+"_part%d.fa"%(cnt),'w')
+            r.write(id+seq)
+            curlen+= len(seq)
+    r.close()

a4cf1f943cc52064271fdf6ca4ba2de479993f16
Todd Treangen <treangen@gmail.com>
2013-02-04 18:01:48 -0500

    Skip Functional Annotate if no DB present

--- src/fannotate.py
+++ src/fannotate.py
@@ -24,11 +24,16 @@ def FunctionalAnnotation(input,output):
       run_process(_settings, "touch %s/FunctionalAnnotation/out/blast.out"%(_settings.rundir), "FunctionalAnnotation")
       return 0
    # uniprot_sprot_enz_set
-   run_process(_settings,"%s/blastall -p blastp -i %s/FindORFS/out/proba.faa -d %s/DB/uniprot_sprot.fasta -a %s -e 0.001 -m 8 -b 1 > %s/FunctionalAnnotation/out/blast.out"%(_settings.BLAST,_settings.rundir,_settings.METAMOS_UTILS,_settings.threads,_settings.rundir),"FunctionalAnnotation")
+   
+   if os.path.exists("%s/DB/uniprot_sprot.fasta"%(_settings.METAMOS_UTILS)):
+       
+       run_process(_settings,"%s/blastall -p blastp -i %s/FindORFS/out/proba.faa -d %s/DB/uniprot_sprot.fasta -a %s -e 0.001 -m 8 -b 1 > %s/FunctionalAnnotation/out/blast.out"%(_settings.BLAST,_settings.rundir,_settings.METAMOS_UTILS,_settings.threads,_settings.rundir),"FunctionalAnnotation")
    #run_process(_settings,"%s/blastall -p blastx -a %d -m 8 -b 1 -e 1e-2 -i %s -d %s/perl/metaphyler/test/test.ref.protein > %s/Annotate/out/%s.query.blastx"%(_settings.BLAST,_settings.threads,orfFA,_settings.METAMOS_UTILS,_settings.rundir,_settings.PREFIX))
    #create index of EC codes
-   ecdata = open("%s/DB/uniprot_sprot_enz_set"%(_settings.METAMOS_UTILS),'r')
-   eclines = ecdata.readlines()
+   eclines = []
+   if os.path.exists("%s/DB/uniprot_sprot_enz_set"%(_settings.METAMOS_UTILS)):
+       ecdata = open("%s/DB/uniprot_sprot_enz_set"%(_settings.METAMOS_UTILS),'r')
+       eclines = ecdata.readlines()
    ecdict = {}
    for line in eclines:
        line = line.replace("\n","")

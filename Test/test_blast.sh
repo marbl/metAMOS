6c5f06851bf71eff6ab757702c870d0382223425
Todd Treangen <treangen@gmail.com>
2013-07-29 16:15:23 -0400

    blast test script

---
+++ Test/test_blast.sh
@@ -0,0 +1,2 @@
+../initPipeline -l flx -f -m carsonella_pe_filt.fna -d test_blast -i 3000:4000 
+../runPipeline -r -v -c blast -p 8 -a soap -d test_blast -k 55 -t -f Preprocess,Assemble,Annotate,Postprocess -z phylum

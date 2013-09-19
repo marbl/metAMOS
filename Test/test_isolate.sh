0dbd088a59f6d00ce52f70262776f39ec680b2d3
Sergey Koren <sergek@umd.edu>
2013-09-19 15:48:08 -0400

    Add new testcase for workflows

--- Test/test_isolate.sh
+++ Test/test_isolate.sh
@@ -1,3 +1,3 @@
 #bzip2 -d test4.sff.bz2
-../initPipeline -l flx -f -m carsonella_pe_filt.fna -c test.asm -c carsonella_reference.fna -d test_isolate -i 3000:4000 
-../runPipeline -u -r -v -I -c kraken -p 8 -a masurca,soap,velvet,ray -d test_isolate -k 51 -t -n FunctionalAnnotation -f MapReads,Postprocess -z phylum
+../initPipeline -d test_isolate -W isolate_test
+../runPipeline -p 8 -k 51 -d test_isolate

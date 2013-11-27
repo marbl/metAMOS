8873a0247e5901ca17b5798638801ca7b7dc7e9d
Sergey Koren <sergek@umd.edu>
2013-11-27 17:05:04 -0500

    Add cat to list of commands allowed to return non-0 code. Skip read annotation for metaphyler

--- src/utils.py
+++ src/utils.py
@@ -1051,7 +1051,7 @@ def run_process(settings,command,step=""):
               p = subprocess.Popen(command, shell=True, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE,close_fds=True,executable="/bin/bash", cwd=workingDir)
           fstdout,fstderr = p.communicate()
           rc = p.returncode
-          if rc != 0 and _failFast and "rm " not in command and "ls " not in command and "unlink " not in command and "ln " not in command and "mkdir " not in command and "mv " not in command:
+          if rc != 0 and _failFast and "rm " not in command and "ls " not in command and "unlink " not in command and "ln " not in command and "mkdir " not in command and "mv " not in command and "cat" not in command:
               # flush all error/output streams
               outf.flush()
               outf.write(fstdout+fstderr)

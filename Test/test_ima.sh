8c033c0ad520540aebf300a703f15a2f272d07f1
Sergey Koren <sergek@umd.edu>
2014-02-08 18:58:11 -0500

    Renamed test case

---
+++ Test/test_ima.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+../initPipeline -d test_ima -W test_ima
+../runPipeline -d test_ima -p 8 
--- Test/test_isolate.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-../initPipeline -d test_isolate -W test_isolate
-../runPipeline -d test_isolate -p 8 

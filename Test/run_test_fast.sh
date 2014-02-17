3ee6f31f8a297e9ffd360ba466a9e4b7d621c710
Sergey Koren <sergek@umd.edu>
2014-02-17 18:54:06 -0500

    Fix runPipeline path

--- Test/run_test_fast.sh
+++ Test/run_test_fast.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 ../initPipeline -f -m carsonella_pe_filt.fna -d test_fast  -i 500:3500
-../dist/pdfposter/runPipeline  -a soap -c kraken -g fraggenescan -y -p 15 -d test_fast -k 55 -f Assemble,MapReads,FindORFS,FindScaffoldORFS,Abundance,Classify,Annotate
+../runPipeline  -a soap -c kraken -g fraggenescan -y -p 15 -d test_fast -k 55 -f Assemble,MapReads,FindORFS,FindScaffoldORFS,Abundance,Classify,Annotate

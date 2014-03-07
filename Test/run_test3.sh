e5d4ea646b735c864350e410b009d08812b13ffe
Sergey Koren <sergek@umd.edu>
2014-03-06 19:25:14 -0500

    Update test to use contig with dots in name

--- Test/run_test3.sh
+++ Test/run_test3.sh
@@ -3,5 +3,5 @@
 #../runPipeline -c metaphyler -p 8 -d test2 -k 31 -f Scaffold,Propagate,Classify -v
 #now test contigs
 #mv ./test2/Assemble/out/proba.asm.contig test.asm
-../initPipeline -f -m MA_test2.filt2.fna -d test3 -i 1300:1700 -c test.asm
-../runPipeline -q -c fcp -p 8 -d test3 -k 31 -v
+../initPipeline -f -m MA_test2.filt2.fna -d test3 -i 1300:1700 -c test.ctg.asm
+../runPipeline -q -c fcp -p 8 -d test3 -k 31 -v -f FindORFS

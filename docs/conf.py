a79524fa2a39a21a4accb616621b123690fa2caa
Todd Treangen <treangen@gmail.com>
2014-03-25 13:48:50 -0400

    auth info

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MetAMOS'
-copyright = u'2014, Sergey Koren & Todd J Treangen'
+copyright = u'2014, MetAMOS development team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

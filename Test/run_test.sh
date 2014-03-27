aab0d417539b84e73b9e9e08a06dfa3eb9b63ebb
Sergey Koren <sergek@umd.edu>
2014-03-27 13:39:33 -0400

    Renamed test

--- Test/run_test.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-../initPipeline -f -m carsonella_pe_filt.fna -d test1  -i 500:3500
-../runPipeline -a soap -c kraken -g fraggenescan -p 15 -d test1 -v -k 55 -f Assemble,MapReads,FindORFS,Annotate,FunctionalAnnotation,Propagate,Classify,Abundance,FindScaffoldORFS -n FunctionalAnnotation
